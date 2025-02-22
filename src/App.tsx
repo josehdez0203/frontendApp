fc6d5b66b226fcfd3149da8c6125cb1edc8ffeb6
Jose Hernandez Cazarez <josehernandezc@gmail.com>
2025-02-22 13:06:55 -0600

    Inicio de App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button } from "./components/button";
+import "./styles/global.scss";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>My React typescript app</h1>
+      <Button> Click me!</Button>
     </div>
   );
-}
+};
 
 export default App;
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // ... otras configuraciones
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
